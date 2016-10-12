1917923b7d6bb6fd6669524ed0350aad74bd2217
RobertPratley <rob.pratley@gmail.com>
2016-10-12 20:08:06 +0100

    second commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@ mkdir -p $SRC && cd $SRC
 
 echo "Setting up Mac..."
 
+git clone --recursive https://github.com/RobertPratley/Dotfiles.git
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
