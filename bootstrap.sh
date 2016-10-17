099fc65099f468cd7626e25131178df5d8fe25f6
RobertPratley <rob.pratley@gmail.com>
2016-10-17 20:50:42 +0100

    Fixed bootstrap and .macos

--- bootstrap.sh
+++ bootstrap.sh
@@ -49,6 +49,6 @@ read drivesetup
 
 # Symlink application preferences form Google Drive/Configs to /Library/Application support/
 
-rm -r ~/Library/Application\ Support/Alfred\ 3/ && ln -sf ~/Google\ Drive/Configs/Alfred\ 3 ~/Library/Application\ Support/
+rm -r ~/Library/Application\ Support/Alfred\ 3/ && ln -sf ~/Google\ Drive/Configs/Alfred\ 3/ ~/Library/Application\ Support/
 # rm -r ~/Library/Application\ Support/Glyphs && ln -sf ~/Google\ Drive/Configs/Glyphs ~/Library/Application\ Support/
-rm -r ~/Library/Application\ Support/Sublime\ Text\ 3/ && ln -sf ~/Google\ Drive/Configs/Sublime\ Text\ 3 ~/Library/Application\ Support/
+rm -r ~/Library/Application\ Support/Sublime\ Text\ 3/ && ln -sf ~/Google\ Drive/Configs/Sublime\ Text\ 3/ ~/Library/Application\ Support/
